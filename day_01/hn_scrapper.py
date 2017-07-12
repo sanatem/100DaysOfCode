553c2cd3cf0c2726f1bd7912153afadc15d01698
Santiago Pravisani <santiagopravisani@gmail.com>
2017-07-12 00:23:58 -0300

    Day 01: Improvement

--- day_01/hn_scrapper.py
+++ day_01/hn_scrapper.py
@@ -1,21 +1,22 @@
 import requests
 from BeautifulSoup import BeautifulSoup
 
-url = "https://news.ycombinator.com/"
-response = requests.get(url)
-html = response.content
-soup = BeautifulSoup(html)
-table = soup.find('table')
+response = requests.get("https://news.ycombinator.com/")
 
+html = response.content
+
+soup = BeautifulSoup(html)
+
+table = soup.find('table')
 
 #We iterate over the rows
 links = []
 
 for row in table.findAll('tr',attrs={'class':'athing'}):
   for cell in row.findAll('td',attrs={'class':'title'}):
-    link = cell.findAll('a',attrs={'class':'storylink'})
+    link = cell.find('a',attrs={'class':'storylink'})
     if link:
-      links.append({link[0].text.encode('utf-8') : link[0].get('href').encode('utf-8')})
+      links.append({link.text.encode('utf-8') : link.get('href').encode('utf-8')})
 
 for link in links:
   print link
